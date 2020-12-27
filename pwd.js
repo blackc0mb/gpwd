bea974b16ccc93369c328268b462b5a88b009169
Tony <santmd77@gmail.com>
2020-12-27 14:00:14 -0600

    variables
    adjust variable names

--- pwd.js
+++ pwd.js
@@ -1,21 +1,19 @@
 function get_passwords(intNumPwd, intLenght) {
    var strText = "";
    var strRetPasswords = "";
-   var strPossible = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ0123456789._?#$%&=),!-;*";
+   var strChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ0123456789._?#$%&=),!-;*";
 
    for (var intIterations = 1; intIterations <= intNumPwd; intIterations++) {
       strText = "";
-      for (var intCont = 1; intCont <= intLenght; intCont++) {
-         strText += strPossible.charAt(Math.floor(Math.random() * strPossible.length));
+      for (var intCounter = 1; intCounter <= intLenght; intCounter++) {
+         strText += strChars.charAt(Math.floor(Math.random() * strChars.length));
       }      
       strRetPasswords += strText + "<br>"
    }
-   //alert(strRetPasswords);
    document.getElementById('lblresult').innerHTML = strRetPasswords;
-   // return strRetPasswords;
 }
 
 function setText(id,newvalue) {
-   var s= document.getElementById(id);
-   s.innerHTML = newvalue;
+   var newtextelemnt = document.getElementById(id);
+   newtextelemnt.innerHTML = newvalue;
  } 
