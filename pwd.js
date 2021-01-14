ce89c3b9515dd2c42c3e5b99c9b005f695fea6a9
Tony <santmd77@gmail.com>
2021-01-13 19:12:03 -0600

    se cambió diseño y validaciones
    se cambió diseño en tamaño de objetos y validaciones al momento de la captura

--- pwd.js
+++ pwd.js
@@ -1,7 +1,24 @@
-function get_passwords(intNumPwd, intLenght) {
+function validate_form() {
+   if ( parseInt(document.getElementById('numpwd').value) > 50 ) {
+         return false;
+      }
+
+      if ( parseInt(document.getElementById('pwdlenght').value) > 30) {
+         return false;
+      }
+      
+      return true;
+}
+
+function get_passwords(intNumPwd, intLenght) {   
    var strText = "";
    var strRetPasswords = "";
    var strChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTVWXYZ0123456789._?#$%&=),!-;*";
+
+   if (validate_form() == false) {
+      document.getElementById('lblresult').innerHTML = "Output"
+      return strRetPasswords;
+   }   
 
    for (var intIterations = 1; intIterations <= intNumPwd; intIterations++) {
       strText = "";
@@ -10,10 +27,10 @@ function get_passwords(intNumPwd, intLenght) {
       }      
       strRetPasswords += strText + "<br>";
    }
-   document.getElementById('lblresult').innerHTML = strRetPasswords;
+   document.getElementById('lblresult').innerHTML = strRetPasswords;   
 }
 
 function setText(id,newvalue) {
    var newtextelemnt = document.getElementById(id);
    newtextelemnt.innerHTML = newvalue;
- } 
+ }
